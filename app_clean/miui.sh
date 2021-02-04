250ba72e1e56fd2f8d9faeb52c1762aee85c19e8
DeclanShao <74823397+DeclanShao@users.noreply.github.com>
2021-02-04 19:25:33 +0800

    Update miui.sh

--- app_clean/miui.sh
+++ app_clean/miui.sh
@@ -19,3 +19,24 @@ rm -rf $1/data-app/com.dragon.read_104
 rm -rf $1/data-app/com.smile.gifmaker_13
 rm -rf $1/data-app/com.taobao.taobao_24
 
+#fps
+sed -i "/<\/features>/d" $1/etc/device_features/*
+echo '<!-- whether support fps change -->
+    <integer-array name="fpsList">
+        <item>120</item>
+        <item>60</item>
+    </integer-array>
+    <integer name="defaultFps">60</integer>' >> $1/etc/device_features/*
+    
+echo "</features>" >> $1/etc/device_features/*
+mv $1/etc/device_features/* $1/etc/device_features/oppo6853.xml
+
+echo "#property for fps switch mode type
+      ro.vendor.fps.switch.default=true
+      #for CTS fps
+      ro.vendor.display.default_fps=60
+      #property for dfps
+      ro.vendor.smart_dfps.enable=true
+      #property for display fps switch
+      persist.vendor.power.dfps.level=0" >> $1/build.prop
+
