1e6ebac7e1c186cde9acee44959472a4904bc241
DeclanShao <74823397+DeclanShao@users.noreply.github.com>
2021-02-04 16:10:18 +0800

    Update miui.sh

--- app_clean/miui.sh
+++ app_clean/miui.sh
@@ -4,5 +4,18 @@ LOCALDIR=`cd "$( dirname $0 )" && pwd`
 cd $LOCALDIR
 
 #rm -rf $1/data-app/*
+#vndk29 vndk28
 rm -rf $1/system_ext/apex
+
+#data
+rm -rf $1/data-app/com.zhihu.android_28
+rm -rf $1/data-app/Youpin
+rm -rf $1/data-app/VipAccount
+rm -rf $1/data-app/Userguide
+rm -rf $1/data-app/MiShop
+rm -rf $1/data-app/GameCenter
+rm -rf $1/data-app/Email
+rm -rf $1/data-app/com.dragon.read_104
+rm -rf $1/data-app/com.smile.gifmaker_13
+rm -rf $1/data-app/com.taobao.taobao_24
 
